dc95cd47186763d97ec0066803947667e9f5482f
Alex <nothingnicehere@gmail.com>
2023-09-10 00:20:41 -0400

    Fixed test

--- src/server.js
+++ src/server.js
@@ -8,29 +8,29 @@ const imageHandler = require('./imageResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
-    //All cases of different URL additions
+  // All cases of different URL additions
   switch (request.url) {
     case '/':
-        htmlHandler.getIndex(request, response);
-        break;
+      htmlHandler.getIndex(request, response);
+      break;
     case '/page2':
-        htmlHandler.getPage2(request, response);
-        break;
+      htmlHandler.getPage2(request, response);
+      break;
     case '/hello':
-        textHandler.getHello(request, response);
-        break;
+      textHandler.getHello(request, response);
+      break;
     case '/time':
-        textHandler.getTime(request, response);
-        break;
+      textHandler.getTime(request, response);
+      break;
     case '/helloJSON':
-        jsonHandler.getHelloJSON(request, response);
-        break;
+      jsonHandler.getHelloJSON(request, response);
+      break;
     case '/timeJSON':
-        jsonHandler.getTimeJSON(request, response);
-        break;
+      jsonHandler.getTimeJSON(request, response);
+      break;
     case '/dankmemes':
-        imageHandler.getImage(request, response);
-        break;
+      imageHandler.getImage(request, response);
+      break;
     default:
       htmlHandler.getIndex(request, response);
       break;
