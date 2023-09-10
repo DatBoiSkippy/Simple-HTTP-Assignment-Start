82f3596a6254fc90c7c0213e2dab46f0fe2147ac
Alex <nothingnicehere@gmail.com>
2023-09-09 23:55:55 -0400

    Completed HW 1

---
+++ src/htmlResponses.js
@@ -0,0 +1,22 @@
+const fs = require('fs'); // file system module
+
+// dirname allows specification of relative paths from the current file
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const page2 = fs.readFileSync(`${__dirname}/../client/client2.html`);
+
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+const getPage2 = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(page2);
+  response.end();
+};
+
+module.exports = {
+  getIndex,
+  getPage2,
+};
